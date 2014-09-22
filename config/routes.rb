f5f5fb2a8c905ae4328eafd61c9dc1aad444692d
Francis <goulet.francis@gmail.com>
2014-09-22 14:25:26 -0400

    Added cors in app config

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :cors
   match '/', :to => proc {|env| [200, {'Content-Type' => 'text/plain'}, ["Hello world"]] },
              :via => [:get, :post, :put, :delete, :options, :head, :patch]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
